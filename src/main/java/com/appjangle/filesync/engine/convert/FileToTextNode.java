33dcffe21cc9d1f0fa5cb7e37ce81c51c23d1678
mxro <mxro@nowhere.com>
2014-11-14 09:19:19 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 09:19:19 EST 2014

--- src/main/java/com/appjangle/filesync/engine/convert/FileToTextNode.java
+++ src/main/java/com/appjangle/filesync/engine/convert/FileToTextNode.java
@@ -6,12 +6,31 @@ import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.file.FileItem;
 import de.mxro.fn.Closure;
 import io.nextweb.Node;
+import io.nextweb.Query;
+import io.nextweb.promise.exceptions.ExceptionListener;
+import io.nextweb.promise.exceptions.ExceptionResult;
+import java.util.LinkedList;
 import java.util.List;
 
 @SuppressWarnings("all")
 public class FileToTextNode implements FileToNodes {
   public void update(final FileItem source, final Node node, final ValueCallback<List<Closure<NetworkOperationContext>>> cb) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nmismatched input \'}\' expecting \']\'");
+    final String content = source.getText();
+    final LinkedList<Closure<NetworkOperationContext>> ops = new LinkedList<Closure<NetworkOperationContext>>();
+    final Closure<NetworkOperationContext> _function = new Closure<NetworkOperationContext>() {
+      public void apply(final NetworkOperationContext ctx) {
+        Node _node = ctx.node();
+        Query _setValueSafe = _node.setValueSafe(content);
+        final ExceptionListener _function = new ExceptionListener() {
+          public void onFailure(final ExceptionResult er) {
+            Throwable _exception = er.exception();
+            cb.onFailure(_exception);
+          }
+        };
+        _setValueSafe.catchExceptions(_function);
+      }
+    };
+    ops.add(_function);
+    cb.onSuccess(ops);
   }
 }
--- src/main/xtend/com/appjangle/filesync/engine/convert/FileToTextNode.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/convert/FileToTextNode.xtend
@@ -1,7 +1,6 @@
 package com.appjangle.filesync.engine.convert
 
 import com.appjangle.filesync.FileToNodes
-import com.appjangle.filesync.NetworkOperation
 import com.appjangle.filesync.NetworkOperationContext
 import de.mxro.async.callbacks.ValueCallback
 import de.mxro.file.FileItem
@@ -11,21 +10,23 @@ import java.util.LinkedList
 import java.util.List
 
 class FileToTextNode implements FileToNodes {
-	
+
 	override update(FileItem source, Node node, ValueCallback<List<Closure<NetworkOperationContext>>> cb) {
-		
+
 		val content = source.text
-		
+
 		val ops = new LinkedList<Closure<NetworkOperationContext>>
-		
-		ops.add([ ctx |
-			
-			ctx.node.setValueSafe(content).catchExceptions([er| cb.onFailure(er.exception())
-			
-		])
-		
-		cb.onSuccess(ops)
-		
+
+		ops.add(
+			[ctx|
+				ctx.node.setValueSafe(content).catchExceptions(
+					[ er |
+						cb.onFailure(er.exception())
+					])
+					])
+
+		cb.onSuccess(ops);
+
 	}
 	
-}
+	}

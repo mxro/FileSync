283d95c496fd0f99cfd013953ea5f78c6ef0ab96
mxro <mxro@nowhere.com>
2014-11-13 15:22:50 +1100

    Developing first implementation for FileSync Autocommit Thu Nov 13 15:22:50 EST 2014

--- src/main/java/com/appjangle/filesync/engine/metadata/FileItemMetaData.java
+++ src/main/java/com/appjangle/filesync/engine/metadata/FileItemMetaData.java
@@ -1,5 +1,10 @@
 package com.appjangle.filesync.engine.metadata;
 
+import java.util.Date;
+
 @SuppressWarnings("all")
 public class FileItemMetaData {
+  private String name;
+  
+  private Date lastModified;
 }
--- src/main/xtend/com/appjangle/filesync/engine/metadata/FileItemMetaData.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/metadata/FileItemMetaData.xtend
@@ -1,8 +1,12 @@
 package com.appjangle.filesync.engine.metadata
 
+import java.util.Date
+
 class FileItemMetaData {
 	
+	var String name
 	
+	var Date lastModified
 	
 	
 }

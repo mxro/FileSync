c3d942531f6d5f6e62bc9d766d162710c9c44e17
mxro <mxro@nowhere.com>
2014-11-14 09:09:11 +1100

    a

---
+++ src/main/java/com/appjangle/filesync/engine/convert/FileToTextNode.java
@@ -0,0 +1,14 @@
+package com.appjangle.filesync.engine.convert;
+
+import com.appjangle.filesync.FileToNodes;
+import com.appjangle.filesync.NetworkOperation;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.file.FileItem;
+import io.nextweb.Node;
+import java.util.List;
+
+@SuppressWarnings("all")
+public class FileToTextNode implements FileToNodes {
+  public void update(final FileItem source, final Node node, final ValueCallback<List<NetworkOperation>> cb) {
+  }
+}
---
+++ src/main/xtend/com/appjangle/filesync/engine/convert/FileToTextNode.xtend
@@ -0,0 +1,17 @@
+package com.appjangle.filesync.engine.convert
+
+import com.appjangle.filesync.FileToNodes
+import de.mxro.file.FileItem
+import io.nextweb.Node
+import de.mxro.async.callbacks.ValueCallback
+import java.util.List
+import com.appjangle.filesync.NetworkOperation
+
+class FileToTextNode implements FileToNodes {
+	
+	override update(FileItem source, Node node, ValueCallback<List<NetworkOperation>> cb) {
+		
+		
+	}
+	
+}

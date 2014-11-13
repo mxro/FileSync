a9204403393822c58b915fa631bcde4ee968b221
mxro <mxro@nowhere.com>
2014-11-13 14:04:26 +1100

    Setting up project for FileSync Autocommit Thu Nov 13 14:04:26 EST 2014

--- src/main/java/com/appjangle/filesync/engine/FolderSynchronization.java
+++ src/main/java/com/appjangle/filesync/engine/FolderSynchronization.java
@@ -1,5 +1,9 @@
 package com.appjangle.filesync.engine;
 
+import io.nextweb.Node;
+import java.io.File;
+
 @SuppressWarnings("all")
 public interface FolderSynchronization {
+  public abstract void nodeToFolder(final Node node, final File file);
 }
--- src/main/xtend/com/appjangle/filesync/engine/FolderSynchronization.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/FolderSynchronization.xtend
@@ -1,7 +1,10 @@
 package com.appjangle.filesync.engine
 
+import io.nextweb.Node
+import java.io.File
+
 interface FolderSynchronization {
 	
-	def 
+	def void nodeToFolder(Node node, File file)
 	
 }

7f7ea139478b21c696bc484278c699d762bdfbdd
mxro <mxro@nowhere.com>
2014-11-14 09:14:19 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 09:14:19 EST 2014

--- src/main/java/com/appjangle/filesync/NetworkOperationContext.java
+++ src/main/java/com/appjangle/filesync/NetworkOperationContext.java
@@ -1,5 +1,11 @@
 package com.appjangle.filesync;
 
+import io.nextweb.Node;
+import io.nextweb.Session;
+
 @SuppressWarnings("all")
 public interface NetworkOperationContext {
+  public abstract Session session();
+  
+  public abstract Node node();
 }
--- src/main/xtend/com/appjangle/filesync/NetworkOperationContext.xtend
+++ src/main/xtend/com/appjangle/filesync/NetworkOperationContext.xtend
@@ -1,5 +1,9 @@
 package com.appjangle.filesync
 
+import io.nextweb.Node
+import io.nextweb.Session
+
 interface NetworkOperationContext {
-	
+	def Session session()
+	def Node node()
 }

3634bc9444d6abeb1a637e8a91216b17916ed0d4
mxro <mxro@nowhere.com>
2014-11-14 14:00:20 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 14:00:20 EST 2014

--- src/main/java/com/appjangle/filesync/engine/metadata/Metadata.java
+++ src/main/java/com/appjangle/filesync/engine/metadata/Metadata.java
@@ -1,6 +1,7 @@
 package com.appjangle.filesync.engine.metadata;
 
 import com.appjangle.filesync.engine.metadata.ItemMetadata;
+import io.nextweb.Node;
 import java.util.List;
 
 @SuppressWarnings("all")
@@ -8,6 +9,8 @@ public interface Metadata {
   public abstract List<ItemMetadata> getChildren();
   
   public abstract ItemMetadata getChild(final String name);
+  
+  public abstract ItemMetadata getChild(final Node forNode);
   
   public abstract Metadata add(final ItemMetadata itemMetadata);
 }
--- src/main/xtend/com/appjangle/filesync/engine/metadata/Metadata.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/metadata/Metadata.xtend
@@ -1,5 +1,6 @@
 package com.appjangle.filesync.engine.metadata
 
+import io.nextweb.Node
 import java.util.List
 
 interface Metadata {
@@ -8,6 +9,8 @@ interface Metadata {
 	def List<ItemMetadata> getChildren()
 	
 	def ItemMetadata getChild(String name)
+	
+	def ItemMetadata getChild(Node forNode)
 	
 	def Metadata add(ItemMetadata itemMetadata)
 	

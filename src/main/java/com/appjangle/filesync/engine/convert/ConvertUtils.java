f3cdfb304436e67591b4c7e52f935ddc7bb9c25b
mxro <mxro@nowhere.com>
2014-11-14 12:16:20 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 12:16:20 EST 2014

--- src/main/java/com/appjangle/filesync/engine/convert/ConvertUtils.java
+++ src/main/java/com/appjangle/filesync/engine/convert/ConvertUtils.java
@@ -1,5 +1,15 @@
 package com.appjangle.filesync.engine.convert;
 
+import io.nextweb.Link;
+import io.nextweb.Query;
+import io.nextweb.Session;
+
 @SuppressWarnings("all")
 public class ConvertUtils {
+  public static Query appendLabel(final Query toNode, final String label) {
+    Query _appendSafe = toNode.appendSafe(label);
+    Session _session = toNode.session();
+    Link _link = _session.link("https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel");
+    return _appendSafe.appendSafe(_link, "./label");
+  }
 }
--- src/main/xtend/com/appjangle/filesync/engine/convert/ConvertUtils.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/convert/ConvertUtils.xtend
@@ -1,5 +1,13 @@
 package com.appjangle.filesync.engine.convert
 
+import io.nextweb.Query
+
 class ConvertUtils {
+	
+	
+	def static appendLabel(Query toNode, String label) {
+		toNode.appendSafe(label).appendSafe(
+						toNode.session().link('https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel'), "./label")
+	}
 	
 }

6570047203d249b57c25a87bc977f8a72efc0736
mxro <mxro@nowhere.com>
2014-11-14 14:57:20 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 14:57:20 EST 2014

--- src/main/java/com/appjangle/filesync/engine/SyncFolder.java
+++ src/main/java/com/appjangle/filesync/engine/SyncFolder.java
@@ -1,19 +1,15 @@
 package com.appjangle.filesync.engine;
 
+import com.appjangle.filesync.NetworkOperation;
+import de.mxro.file.FileItem;
+import io.nextweb.Node;
+import java.util.ArrayList;
+
 @SuppressWarnings("all")
 public class SyncFolder {
-  public Object doIt() {
+  public ArrayList<NetworkOperation> doIt(final FileItem folder, final Node node) {
     throw new Error("Unresolved compilation problems:"
-      + "\nThe method or field folder is undefined for the type SyncFolder"
       + "\nThe method or field nodes is undefined for the type SyncFolder"
-      + "\nThe method or field MetadataUtilsJre is undefined for the type SyncFolder"
-      + "\nThe method or field nodes is undefined for the type SyncFolder"
-      + "\nArrayList cannot be resolved."
-      + "\nNetworkOperation cannot be resolved to a type."
-      + "\nassertFolder cannot be resolved"
-      + "\nvisible cannot be resolved"
-      + "\nreadFromFile cannot be resolved"
-      + "\ngetChild cannot be resolved"
       + "\n== cannot be resolved");
   }
 }
--- src/main/xtend/com/appjangle/filesync/engine/SyncFolder.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/SyncFolder.xtend
@@ -1,13 +1,20 @@
 package com.appjangle.filesync.engine
 
+import com.appjangle.filesync.NetworkOperation
+import com.appjangle.filesync.engine.metadata.Metadata
+import com.appjangle.filesync.engine.metadata.MetadataUtilsJre
+import de.mxro.file.FileItem
+import io.nextweb.Node
+import java.util.ArrayList
+
 class SyncFolder {
 	
-	def doIt() {
-		val metadata = folder.assertFolder(".filesync-meta")
+	def doIt(FileItem folder, Node node) {
+		val metadataFolder = folder.assertFolder(".filesync-meta")
 		
-		metadata.visible = false;
+		metadataFolder.visible = false;
 
-		nodes = MetadataUtilsJre.readFromFile(metadata.getChild("nodes.xml"))
+		val metadata = MetadataUtilsJre.readFromFile(metadataFolder.getChild("nodes.xml"))
 
 		if (nodes == null) {
 			return new ArrayList<NetworkOperation>(0)

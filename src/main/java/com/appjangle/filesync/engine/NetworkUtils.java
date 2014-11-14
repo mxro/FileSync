03b1fd2b6cd71e78da02fe15365a8d0cbfb99b46
mxro <mxro@nowhere.com>
2014-11-14 15:18:20 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 15:18:20 EST 2014

--- src/main/java/com/appjangle/filesync/engine/NetworkUtils.java
+++ src/main/java/com/appjangle/filesync/engine/NetworkUtils.java
@@ -1,13 +1,27 @@
 package com.appjangle.filesync.engine;
 
 import com.appjangle.filesync.NetworkOperation;
+import com.appjangle.filesync.NetworkOperationContext;
 import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.fn.Success;
+import io.nextweb.Node;
+import io.nextweb.Session;
+import io.nextweb.promise.Deferred;
 import java.util.List;
 
 @SuppressWarnings("all")
 public class NetworkUtils {
-  public Object execute(final List<NetworkOperation> ops, final ValueCallback<Success> cb) {
-    return null;
+  public void execute(final List<NetworkOperation> ops, final Node onNode, final ValueCallback<Success> cb) {
+    for (final NetworkOperation op : ops) {
+      final List<Deferred<?>> qries = op.apply(new NetworkOperationContext() {
+        public Session session() {
+          return onNode.session();
+        }
+        
+        public Node parent() {
+          return onNode;
+        }
+      });
+    }
   }
 }
--- src/main/xtend/com/appjangle/filesync/engine/NetworkUtils.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/NetworkUtils.xtend
@@ -1,14 +1,35 @@
 package com.appjangle.filesync.engine
 
 import com.appjangle.filesync.NetworkOperation
+import com.appjangle.filesync.NetworkOperationContext
 import de.mxro.async.callbacks.ValueCallback
 import de.mxro.fn.Success
+import io.nextweb.Node
 import java.util.List
 
 class NetworkUtils {
-	
-	
-	def execute(List<NetworkOperation> ops, ValueCallback<Success> cb) {
+
+	def execute(List<NetworkOperation> ops, Node onNode, ValueCallback<Success> cb) {
+		
+		
+		for (NetworkOperation op : ops) {
+			
+			
+			val qries = op.apply(new NetworkOperationContext() {
+				
+				override session() {
+					onNode.session()
+				}
+				
+				override parent() {
+					onNode
+				}
+				
+			})
+			
+			
+			
+		}
 		
 	}
 	

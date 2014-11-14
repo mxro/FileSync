62e9ec54c9bae8ee15a6aa5e9ba935113b8c0747
mxro <mxro@nowhere.com>
2014-11-14 12:29:20 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 12:29:20 EST 2014

--- src/main/java/com/appjangle/filesync/engine/convert/ConvertUtils.java
+++ src/main/java/com/appjangle/filesync/engine/convert/ConvertUtils.java
@@ -1,15 +1,23 @@
 package com.appjangle.filesync.engine.convert;
 
-import io.nextweb.Link;
+import com.google.common.collect.Lists;
+import de.mxro.async.callbacks.ValueCallback;
+import io.nextweb.Node;
 import io.nextweb.Query;
-import io.nextweb.Session;
+import java.util.Collections;
+import java.util.List;
 
 @SuppressWarnings("all")
 public class ConvertUtils {
+  private final List<String> labelTypes = Collections.<String>unmodifiableList(Lists.<String>newArrayList("https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel"));
+  
   public Query appendLabel(final Query toNode, final String label) {
-    Query _appendSafe = toNode.appendSafe(label);
-    Session _session = toNode.session();
-    Link _link = _session.link("https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel");
-    return _appendSafe.appendSafe(_link, "./label");
+    throw new Error("Unresolved compilation problems:"
+      + "\nInvalid number of arguments. The field labelTypes is not applicable for the arguments ((Object)=>int)"
+      + "\nType mismatch: cannot convert from List<String> to Link");
+  }
+  
+  public Object getFileName(final Node fromNode, final ValueCallback<String> cb) {
+    return null;
   }
 }
--- src/main/xtend/com/appjangle/filesync/engine/convert/ConvertUtils.xtend
+++ src/main/xtend/com/appjangle/filesync/engine/convert/ConvertUtils.xtend
@@ -1,13 +1,21 @@
 package com.appjangle.filesync.engine.convert
 
+import de.mxro.async.callbacks.ValueCallback
+import io.nextweb.Node
 import io.nextweb.Query
 
 class ConvertUtils {
 	
+	val labelTypes = #['https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel']
 	
 	def appendLabel(Query toNode, String label) {
 		toNode.appendSafe(label).appendSafe(
-						toNode.session().link('https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/shortLabel'), "./label")
+						toNode.session().link(labelTypes[0]), "./label")
+	}
+	
+	
+	def getFileName(Node fromNode, ValueCallback<String> cb) {
+		
 	}
 	
 }

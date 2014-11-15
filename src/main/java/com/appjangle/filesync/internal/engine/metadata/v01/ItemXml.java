0ebe85dcf9c58968081c783dee1247defe2b1996
mxro <mxro@nowhere.com>
2014-11-15 11:06:28 +1100

    Developing first implementation for FileSync Autocommit Sat Nov 15 11:06:28 EST 2014

--- src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.java
+++ src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.java
@@ -1,5 +1,37 @@
 package com.appjangle.filesync.internal.engine.metadata.v01;
 
+import com.appjangle.filesync.ItemMetadata;
+import java.util.Date;
+
 @SuppressWarnings("all")
-public class ItemXml {
+public class ItemXml implements ItemMetadata {
+  public String name;
+  
+  public Date lastModified;
+  
+  public String uri;
+  
+  public String hash;
+  
+  public String converter;
+  
+  public String name() {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  public Date lastModified() {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  public String uri() {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  public String hash() {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  public String converter() {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
 }
--- src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.xtend
+++ src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.xtend
@@ -1,5 +1,35 @@
 package com.appjangle.filesync.internal.engine.metadata.v01
 
-class ItemXml {
+import com.appjangle.filesync.ItemMetadata
+import java.util.Date
+
+class ItemXml implements ItemMetadata {
+	
+	
+	public var String name;
+	public var Date lastModified;
+	public var String uri;
+	public var String hash;
+	public var String converter;
+	
+	override name() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
+	
+	override lastModified() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
+	
+	override uri() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
+	
+	override hash() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
+	
+	override converter() {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
 	
 }

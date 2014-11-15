b30b6a4471625e9fd70f5282ab2d6d2c1dd93d28
mxro <mxro@nowhere.com>
2014-11-15 14:01:29 +1100

    Developing first synchronization unit test and resolving issues Autocommit Sat Nov 15 14:01:29 EST 2014

--- src/test/java/com/appjangle/filesync/tests/TestSingleFolderSync1.java
+++ src/test/java/com/appjangle/filesync/tests/TestSingleFolderSync1.java
@@ -1,10 +1,17 @@
 package com.appjangle.filesync.tests;
 
+import com.appjangle.jre.AppjangleJre;
+import de.mxro.fn.Success;
+import io.nextweb.common.LocalServer;
+import io.nextweb.promise.NextwebPromise;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestSingleFolderSync1 {
   @Test
   public void test() {
+    final LocalServer testServer = AppjangleJre.startServer();
+    NextwebPromise<Success> _shutdown = testServer.shutdown();
+    _shutdown.get();
   }
 }
--- src/test/xtend/com/appjangle/filesync/tests/TestSingleFolderSync1.xtend
+++ src/test/xtend/com/appjangle/filesync/tests/TestSingleFolderSync1.xtend
@@ -1,11 +1,18 @@
 package com.appjangle.filesync.tests
 
+import com.appjangle.jre.AppjangleJre
 import org.junit.Test
 
 class TestSingleFolderSync1 {
 	
 	@Test
 	def void test() {
+		
+		val testServer = AppjangleJre.startServer()
+		
+		
+		
+		testServer.shutdown.get
 		
 	}
 	

3869cd0b4f4b94a0715d55fc31af3b4caeecf0c1
mxro <mxro@nowhere.com>
2014-11-15 18:45:29 +1100

    Developing first synchronization unit test for FileSync and resolving issues Autocommit Sat Nov 15 18:45:29 EST 2014

--- src/test/java/com/appjangle/filesync/tests/CheckUpdatesTemplate.java
+++ src/test/java/com/appjangle/filesync/tests/CheckUpdatesTemplate.java
@@ -1,9 +1,49 @@
 package com.appjangle.filesync.tests;
 
+import com.appjangle.filesync.FileSync;
 import com.appjangle.filesync.tests.CheckNodesToFilesTemplate;
+import de.mxro.async.Deferred;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
+import de.mxro.fn.Success;
+import io.nextweb.promise.NextwebPromise;
 
 @SuppressWarnings("all")
 public abstract class CheckUpdatesTemplate extends CheckNodesToFilesTemplate {
+  protected abstract void updateNodes();
+  
+  protected abstract void assertFilesAfterUpdate();
+  
+  protected abstract void updateFiles();
+  
+  protected abstract void assertNodesAfterUpdate();
+  
   public void test() {
+    this.defineData();
+    NextwebPromise<Success> _commit = this.session.commit();
+    _commit.get();
+    final Deferred<Success> _function = new Deferred<Success>() {
+      public void get(final ValueCallback<Success> cb) {
+        FileSync.sync(CheckUpdatesTemplate.this.target, CheckUpdatesTemplate.this.source, cb);
+      }
+    };
+    AsyncJre.<Success>waitFor(_function);
+    this.assertFiles();
+    this.updateNodes();
+    final Deferred<Success> _function_1 = new Deferred<Success>() {
+      public void get(final ValueCallback<Success> cb) {
+        FileSync.sync(CheckUpdatesTemplate.this.target, CheckUpdatesTemplate.this.source, cb);
+      }
+    };
+    AsyncJre.<Success>waitFor(_function_1);
+    this.assertFilesAfterUpdate();
+    this.updateFiles();
+    final Deferred<Success> _function_2 = new Deferred<Success>() {
+      public void get(final ValueCallback<Success> cb) {
+        FileSync.sync(CheckUpdatesTemplate.this.target, CheckUpdatesTemplate.this.source, cb);
+      }
+    };
+    AsyncJre.<Success>waitFor(_function_2);
+    this.assertNodesAfterUpdate();
   }
 }
--- src/test/xtend/com/appjangle/filesync/tests/CheckUpdatesTemplate.xtend
+++ src/test/xtend/com/appjangle/filesync/tests/CheckUpdatesTemplate.xtend
@@ -1,9 +1,43 @@
 package com.appjangle.filesync.tests
 
+import com.appjangle.filesync.FileSync
+import de.mxro.async.jre.AsyncJre
+
 abstract class CheckUpdatesTemplate extends CheckNodesToFilesTemplate {
 	
+	protected abstract def void updateNodes()
+	
+	protected abstract def void assertFilesAfterUpdate()
+	
+	protected abstract def void updateFiles()
+	
+	protected abstract def void assertNodesAfterUpdate()
+	
 	override test() {
+		defineData
+		session.commit.get
+
+		AsyncJre.waitFor [cb |
+			FileSync.sync(target, source, cb)
+		]
 		
+		assertFiles
+		
+		updateNodes
+		
+		AsyncJre.waitFor [cb |
+			FileSync.sync(target, source, cb)
+		]
+		
+		assertFilesAfterUpdate
+		
+		updateFiles
+		
+		AsyncJre.waitFor [cb |
+			FileSync.sync(target, source, cb)
+		]
+		
+		assertNodesAfterUpdate
 	}
 	
 }

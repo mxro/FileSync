754301a95b4abac58d3fddde1cb26b8bd1edfe77
mxro <mxro@nowhere.com>
2014-11-15 14:04:29 +1100

    Developing first synchronization unit test and resolving issues Autocommit Sat Nov 15 14:04:29 EST 2014

--- src/test/java/com/appjangle/filesync/tests/TestSingleFolderSync1.java
+++ src/test/java/com/appjangle/filesync/tests/TestSingleFolderSync1.java
@@ -2,17 +2,27 @@ package com.appjangle.filesync.tests;
 
 import com.appjangle.jre.AppjangleJre;
 import de.mxro.fn.Success;
+import io.nextweb.Query;
 import io.nextweb.Session;
 import io.nextweb.common.LocalServer;
 import io.nextweb.promise.NextwebPromise;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 @SuppressWarnings("all")
 public class TestSingleFolderSync1 {
+  @Rule
+  public TemporaryFolder folder = new TemporaryFolder();
+  
   @Test
   public void test() {
     final LocalServer server = AppjangleJre.startServer();
     final Session session = AppjangleJre.createSession(server);
+    final Query data = session.seed(server);
+    data.append("Just a node");
+    NextwebPromise<Success> _commit = session.commit();
+    _commit.get();
     NextwebPromise<Success> _close = session.close();
     _close.get();
     NextwebPromise<Success> _shutdown = server.shutdown();
--- src/test/xtend/com/appjangle/filesync/tests/TestSingleFolderSync1.xtend
+++ src/test/xtend/com/appjangle/filesync/tests/TestSingleFolderSync1.xtend
@@ -1,25 +1,32 @@
 package com.appjangle.filesync.tests
 
 import com.appjangle.jre.AppjangleJre
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 
 class TestSingleFolderSync1 {
-	
+
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+
 	@Test
 	def void test() {
-		
+
 		val server = AppjangleJre.startServer()
-		
-		
+
 		val session = AppjangleJre.createSession(server)
-		
-		
-		
-		
+
+		val data = session.seed(server)
+
+		data.append("Just a node")
+
+		session.commit.get
+
 		session.close.get
-		
+
 		server.shutdown.get
-		
+
 	}
-	
+
 }

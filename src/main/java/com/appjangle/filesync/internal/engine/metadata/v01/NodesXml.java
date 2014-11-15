6b801f6673d642174c1b916ed68c9d9ccf19ac80
mxro <mxro@nowhere.com>
2014-11-15 11:10:28 +1100

    Developing first implementation for FileSync Autocommit Sat Nov 15 11:10:28 EST 2014

--- src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/NodesXml.java
+++ src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/NodesXml.java
@@ -1,7 +1,17 @@
 package com.appjangle.filesync.internal.engine.metadata.v01;
 
+import com.appjangle.filesync.internal.engine.metadata.v01.ItemXml;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 
 @SuppressWarnings("all")
 public class NodesXml implements Serializable {
+  public final List<ItemXml> items;
+  
+  public NodesXml() {
+    ArrayList<ItemXml> _newArrayList = CollectionLiterals.<ItemXml>newArrayList();
+    this.items = _newArrayList;
+  }
 }
--- src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/NodesXml.xtend
+++ src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/NodesXml.xtend
@@ -1,10 +1,14 @@
 package com.appjangle.filesync.internal.engine.metadata.v01
 
 import java.io.Serializable
+import java.util.List
 
 class NodesXml implements Serializable {
 	
+	public val List<ItemXml> items
 	
-	
+	new() {
+		items = newArrayList
+	}
 	
 }

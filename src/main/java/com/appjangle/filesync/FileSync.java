84d4de163740a8acae11bb75b56216122fa06794
mxro <mxro@nowhere.com>
2014-11-15 13:54:29 +1100

    Developing first implementation for FileSync Autocommit Sat Nov 15 13:54:29 EST 2014

--- src/main/java/com/appjangle/filesync/FileSync.java
+++ src/main/java/com/appjangle/filesync/FileSync.java
@@ -1,11 +1,29 @@
 package com.appjangle.filesync;
 
+import com.appjangle.filesync.internal.engine.SyncFolder;
+import com.appjangle.filesync.internal.engine.convert.ConverterCollection;
+import com.appjangle.filesync.internal.engine.convert.FileToTextNode;
+import com.appjangle.filesync.internal.engine.convert.FolderToNode;
 import de.mxro.file.FileItem;
-import nx.core.nodes.v01.Node;
+import io.nextweb.Node;
 
 @SuppressWarnings("all")
 public class FileSync {
-  public static Object sync(final FileItem folder, final Node node) {
-    return null;
+  public static SyncFolder sync(final FileItem folder, final Node node) {
+    ConverterCollection _createDefaultConverter = FileSync.createDefaultConverter();
+    return new SyncFolder(folder, node, _createDefaultConverter);
+  }
+  
+  public static ConverterCollection createDefaultConverter() {
+    ConverterCollection _xblockexpression = null;
+    {
+      final ConverterCollection coll = new ConverterCollection();
+      FileToTextNode _fileToTextNode = new FileToTextNode();
+      coll.addConverter(_fileToTextNode);
+      FolderToNode _folderToNode = new FolderToNode();
+      coll.addConverter(_folderToNode);
+      _xblockexpression = coll;
+    }
+    return _xblockexpression;
   }
 }
--- src/main/xtend/com/appjangle/filesync/FileSync.xtend
+++ src/main/xtend/com/appjangle/filesync/FileSync.xtend
@@ -1,11 +1,29 @@
 package com.appjangle.filesync
 
+import com.appjangle.filesync.internal.engine.SyncFolder
+import com.appjangle.filesync.internal.engine.convert.ConverterCollection
+import com.appjangle.filesync.internal.engine.convert.FileToTextNode
+import com.appjangle.filesync.internal.engine.convert.FolderToNode
 import de.mxro.file.FileItem
-import nx.core.nodes.v01.Node
+import io.nextweb.Node
 
 class FileSync {
 	
 	def static sync(FileItem folder, Node node) {
+		
+		new SyncFolder(folder, node, createDefaultConverter)
+		
+	}
+	
+	
+	def static createDefaultConverter() {
+		
+		val coll = new ConverterCollection
+		
+		coll.addConverter(new FileToTextNode)
+		coll.addConverter(new FolderToNode)
+		
+		coll
 		
 	}
 	

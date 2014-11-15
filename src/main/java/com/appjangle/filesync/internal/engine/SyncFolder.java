54421e6173e9d16a9736d05b0cbbb0718ed1c667
mxro <mxro@nowhere.com>
2014-11-15 20:36:29 +1100

    Developing first synchronization unit test for FileSync and resolving issues Autocommit Sat Nov 15 20:36:29 EST 2014

--- src/main/java/com/appjangle/filesync/internal/engine/SyncFolder.java
+++ src/main/java/com/appjangle/filesync/internal/engine/SyncFolder.java
@@ -16,6 +16,7 @@ import de.mxro.fn.Success;
 import io.nextweb.Node;
 import java.util.List;
 import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class SyncFolder {
@@ -49,6 +50,7 @@ public class SyncFolder {
       public void apply(final List<NetworkOperation> ops) {
         final Closure<Success> _function = new Closure<Success>() {
           public void apply(final Success it) {
+            InputOutput.<String>println("executed network ops");
             SyncFolder.this.download(cb);
           }
         };
@@ -64,6 +66,7 @@ public class SyncFolder {
     NetworkToFileOperations _networkToFileOperations = new NetworkToFileOperations(this.node, this.folder, this.metadata, this.converter);
     final Closure<List<FileOperation>> _function = new Closure<List<FileOperation>>() {
       public void apply(final List<FileOperation> ops) {
+        InputOutput.<String>println("about to execute file ops");
         SyncFolder.this.fileUtils.execute(ops, SyncFolder.this.folder, SyncFolder.this.metadata);
         SyncFolder.this.fileUtils.saveForFolder(SyncFolder.this.metadata, SyncFolder.this.folder);
         cb.onSuccess(Success.INSTANCE);
--- src/main/xtend/com/appjangle/filesync/internal/engine/SyncFolder.xtend
+++ src/main/xtend/com/appjangle/filesync/internal/engine/SyncFolder.xtend
@@ -37,7 +37,7 @@ class SyncFolder {
 		
 		new FileToNetworkOperations(node, folder, metadata, converter).determineOps(cb.embed [ ops |
 			ops.execute(node, cb.embed [ 
-
+				println('executed network ops')
 				download(cb)
 			])
 		])
@@ -49,6 +49,7 @@ class SyncFolder {
 	
 	def download(ValueCallback<Success> cb) {
 		new NetworkToFileOperations(node, folder, metadata, converter).determineOps(cb.embed([ ops |
+			println('about to execute file ops')
 			ops.execute(folder, metadata)
 			metadata.saveForFolder(folder)
 			cb.onSuccess(Success.INSTANCE)

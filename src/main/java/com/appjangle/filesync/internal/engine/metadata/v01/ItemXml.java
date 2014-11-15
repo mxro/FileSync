1d09bc315337d9fe8c5d9e8c056fafc4d98533bb
mxro <mxro@nowhere.com>
2014-11-15 12:22:29 +1100

    Developing first implementation for FileSync Autocommit Sat Nov 15 12:22:29 EST 2014

--- src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.java
+++ src/main/java/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.java
@@ -3,8 +3,10 @@ package com.appjangle.filesync.internal.engine.metadata.v01;
 import com.appjangle.filesync.ItemMetadata;
 import java.util.Date;
 import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtend.lib.annotations.EqualsHashCode;
 import org.eclipse.xtext.xbase.lib.Pure;
 
+@EqualsHashCode
 @SuppressWarnings("all")
 public class ItemXml implements ItemMetadata {
   @Accessors
@@ -40,6 +42,57 @@ public class ItemXml implements ItemMetadata {
   
   public String converter() {
     return this.converter;
+  }
+  
+  @Override
+  @Pure
+  public boolean equals(final Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ItemXml other = (ItemXml) obj;
+    if (this.name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!this.name.equals(other.name))
+      return false;
+    if (this.lastModified == null) {
+      if (other.lastModified != null)
+        return false;
+    } else if (!this.lastModified.equals(other.lastModified))
+      return false;
+    if (this.uri == null) {
+      if (other.uri != null)
+        return false;
+    } else if (!this.uri.equals(other.uri))
+      return false;
+    if (this.hash == null) {
+      if (other.hash != null)
+        return false;
+    } else if (!this.hash.equals(other.hash))
+      return false;
+    if (this.converter == null) {
+      if (other.converter != null)
+        return false;
+    } else if (!this.converter.equals(other.converter))
+      return false;
+    return true;
+  }
+  
+  @Override
+  @Pure
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.name== null) ? 0 : this.name.hashCode());
+    result = prime * result + ((this.lastModified== null) ? 0 : this.lastModified.hashCode());
+    result = prime * result + ((this.uri== null) ? 0 : this.uri.hashCode());
+    result = prime * result + ((this.hash== null) ? 0 : this.hash.hashCode());
+    result = prime * result + ((this.converter== null) ? 0 : this.converter.hashCode());
+    return result;
   }
   
   @Pure
--- src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.xtend
+++ src/main/xtend/com/appjangle/filesync/internal/engine/metadata/v01/ItemXml.xtend
@@ -3,9 +3,10 @@ package com.appjangle.filesync.internal.engine.metadata.v01
 import com.appjangle.filesync.ItemMetadata
 import java.util.Date
 import org.eclipse.xtend.lib.annotations.Accessors
+import org.eclipse.xtend.lib.annotations.EqualsHashCode
 
+@EqualsHashCode
 class ItemXml implements ItemMetadata {
-	
 	
 	@Accessors String name
 	@Accessors Date lastModified

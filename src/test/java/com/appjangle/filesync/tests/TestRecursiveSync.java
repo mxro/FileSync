7a0e4a42a22cbdd80f9101f85f564ddde3d4cd22
mxro <mxro@nowhere.com>
2014-11-17 15:07:51 +1100

    Testing converting files to nodes Autocommit Mon Nov 17 15:07:51 EST 2014

--- src/test/java/com/appjangle/filesync/tests/TestRecursiveSync.java
+++ src/test/java/com/appjangle/filesync/tests/TestRecursiveSync.java
@@ -1,6 +1,7 @@
 package com.appjangle.filesync.tests;
 
 import com.appjangle.filesync.tests.CheckNodesToFilesTemplate;
+import io.nextweb.Query;
 
 @SuppressWarnings("all")
 public class TestRecursiveSync extends CheckNodesToFilesTemplate {
@@ -9,10 +10,17 @@ public class TestRecursiveSync extends CheckNodesToFilesTemplate {
   }
   
   protected void step1_defineData() {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+    Query _append = this.source.append("oh my");
+    _append.append("And in the subfolder");
+    Query _append_1 = this.source.append("Hello");
+    _append_1.append("Any another foder");
+    final Query node3 = this.source.append("node3");
+    Query _append_2 = node3.append("child1");
+    _append_2.append("b");
+    Query _append_3 = node3.append("child2");
+    _append_3.append("c");
   }
   
   protected void step2_assertFiles() {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
   }
 }
--- src/test/xtend/com/appjangle/filesync/tests/TestRecursiveSync.xtend
+++ src/test/xtend/com/appjangle/filesync/tests/TestRecursiveSync.xtend
@@ -6,13 +6,17 @@ class TestRecursiveSync extends CheckNodesToFilesTemplate{
 		true
 	}
 	
-	
 	override protected step1_defineData() {
-		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+		source.append("oh my").append("And in the subfolder")
+		source.append('Hello').append("Any another foder")
+		
+		val node3 = source.append("node3")
+		node3.append("child1").append("b")
+		node3.append("child2").append("c")
 	}
 	
 	override protected step2_assertFiles() {
-		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+		
 	}
 	
 }

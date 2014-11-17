44b342eff7d60e70286e0cb1984be992f34d84c3
mxro <mxro@nowhere.com>
2014-11-17 13:39:58 +1100

    Testing converting files to nodes Autocommit Mon Nov 17 13:39:58 EST 2014

--- src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
+++ src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
@@ -1,5 +1,83 @@
 package com.appjangle.filesync.tests;
 
+import com.appjangle.filesync.FileSync;
+import com.appjangle.jre.AppjangleJre;
+import de.mxro.async.Deferred;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
+import de.mxro.file.FileItem;
+import de.mxro.file.Jre.FilesJre;
+import de.mxro.fn.Success;
+import io.nextweb.Node;
+import io.nextweb.Query;
+import io.nextweb.Session;
+import io.nextweb.common.LocalServer;
+import io.nextweb.promise.NextwebPromise;
+import java.io.File;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
 @SuppressWarnings("all")
-public class CheckFilesToNodesTemplate {
+public abstract class CheckFilesToNodesTemplate {
+  protected LocalServer server;
+  
+  protected Session session;
+  
+  protected Node source;
+  
+  protected File target;
+  
+  protected FileItem result;
+  
+  @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
+  
+  @Before
+  public void setUp() {
+    try {
+      LocalServer _startServer = AppjangleJre.startServer();
+      this.server = _startServer;
+      Session _createSession = AppjangleJre.createSession(this.server);
+      this.session = _createSession;
+      Query _seed = this.session.seed(this.server);
+      Node _get = _seed.get();
+      this.source = _get;
+      File _newFolder = this.tempFolder.newFolder("sync1");
+      this.target = _newFolder;
+      FileItem _wrap = FilesJre.wrap(this.target);
+      this.result = _wrap;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  protected abstract void step1_defineData();
+  
+  protected abstract void step2_assertFiles();
+  
+  @Test
+  public void test() {
+    this.step1_defineData();
+    NextwebPromise<Success> _commit = this.session.commit();
+    _commit.get();
+    final Deferred<Success> _function = new Deferred<Success>() {
+      public void get(final ValueCallback<Success> cb) {
+        FileSync.sync(CheckFilesToNodesTemplate.this.target, CheckFilesToNodesTemplate.this.source, cb);
+      }
+    };
+    AsyncJre.<Success>waitFor(_function);
+    this.step2_assertFiles();
+  }
+  
+  @After
+  public void tearDown() {
+    NextwebPromise<Success> _close = this.session.close();
+    _close.get();
+    NextwebPromise<Success> _shutdown = this.server.shutdown();
+    _shutdown.get();
+  }
 }
--- src/test/xtend/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.xtend
+++ src/test/xtend/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.xtend
@@ -1,5 +1,66 @@
 package com.appjangle.filesync.tests
 
-class CheckFilesToNodesTemplate {
+import com.appjangle.filesync.FileSync
+import com.appjangle.jre.AppjangleJre
+import de.mxro.async.jre.AsyncJre
+import de.mxro.file.FileItem
+import de.mxro.file.Jre.FilesJre
+import io.nextweb.Node
+import io.nextweb.Session
+import io.nextweb.common.LocalServer
+import java.io.File
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+abstract class CheckFilesToNodesTemplate {
 	
+	protected LocalServer server
+	protected Session session
+	protected Node source
+	protected File target
+	protected FileItem result
+	
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+	
+	@Before
+	def void setUp() {
+		server = AppjangleJre.startServer()
+
+		session = AppjangleJre.createSession(server)
+
+		source = session.seed(server).get
+		
+		target = tempFolder.newFolder("sync1")
+		
+		result = FilesJre.wrap(target)
+	}
+	
+	def protected abstract void step1_defineData()
+	
+	def protected abstract void step2_assertFiles()
+	
+	@Test
+	def void test() {
+		step1_defineData
+		session.commit.get
+
+		AsyncJre.waitFor [cb |
+			FileSync.sync(target, source, cb)
+		]
+		
+		step2_assertFiles
+		
+	}
+	
+	
+	@After
+	def void tearDown() {
+		session.close.get
+
+		server.shutdown.get
+	}
 }

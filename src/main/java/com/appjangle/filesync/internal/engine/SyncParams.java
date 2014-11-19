69f570afcc8ff75ceec4c1458f27028e201500e5
mxro <mxro@nowhere.com>
2014-11-19 12:54:58 +1100

    Allowing to set various parameters for synchronization Autocommit Wed Nov 19 12:54:58 EST 2014

--- src/main/java/com/appjangle/filesync/internal/engine/SyncParams.java
+++ src/main/java/com/appjangle/filesync/internal/engine/SyncParams.java
@@ -1,5 +1,20 @@
 package com.appjangle.filesync.internal.engine;
 
+import com.appjangle.filesync.Converter;
+import com.appjangle.filesync.SynchronizationSettings;
+import com.appjangle.filesync.SynchronizationState;
+import de.mxro.file.FileItem;
+import io.nextweb.Node;
+
 @SuppressWarnings("all")
 public interface SyncParams {
+  public abstract FileItem folder();
+  
+  public abstract Node node();
+  
+  public abstract SynchronizationSettings settings();
+  
+  public abstract SynchronizationState state();
+  
+  public abstract Converter converter();
 }
--- src/main/xtend/com/appjangle/filesync/internal/engine/SyncParams.xtend
+++ src/main/xtend/com/appjangle/filesync/internal/engine/SyncParams.xtend
@@ -1,7 +1,21 @@
 package com.appjangle.filesync.internal.engine
 
+import com.appjangle.filesync.Converter
+import com.appjangle.filesync.SynchronizationSettings
+import com.appjangle.filesync.SynchronizationState
+import de.mxro.file.FileItem
+import io.nextweb.Node
+
 interface SyncParams {
 	
+	def FileItem folder()
 	
+	def Node node()
+	
+	def SynchronizationSettings settings()
+	
+	def SynchronizationState state()
+	
+	def Converter converter()
 	
 }

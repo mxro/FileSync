93639572984a4bbad8f759b201722d44df7aaae7
mxro <mxro@nowhere.com>
2014-11-20 07:26:41 +1100

    a

---
+++ src/main/java/com/appjangle/filesync/SyncNotifications.java
@@ -0,0 +1,10 @@
+package com.appjangle.filesync;
+
+import de.mxro.file.FileItem;
+import io.nextweb.Node;
+
+@SuppressWarnings("all")
+public class SyncNotifications {
+  public void onInsufficientAuthorization(final FileItem inFolder, final Node forNode) {
+  }
+}
---
+++ src/main/xtend/com/appjangle/filesync/SyncNotifications.xtend
@@ -0,0 +1,12 @@
+package com.appjangle.filesync
+
+import de.mxro.file.FileItem
+import io.nextweb.Node
+
+class SyncNotifications {
+	
+	def void onInsufficientAuthorization(FileItem inFolder, Node forNode) {
+		
+	}
+	
+}

dc673954c98ce4ac7faa8e4cba636cda48e50821
mxro <mxro@nowhere.com>
2015-01-11 11:44:11 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 11:44:11 EST 2015

--- src/main/java/com/appjangle/filesync/jre/FileSyncJre.java
+++ src/main/java/com/appjangle/filesync/jre/FileSyncJre.java
@@ -1,24 +1,13 @@
 package com.appjangle.filesync.jre;
 
-import com.appjangle.filesync.FileSync;
-import de.mxro.async.Deferred;
-import de.mxro.async.callbacks.ValueCallback;
-import de.mxro.async.jre.AsyncJre;
-import de.mxro.file.FileItem;
-import de.mxro.file.Jre.FilesJre;
-import de.mxro.fn.Success;
 import io.nextweb.Node;
 import java.io.File;
 
 @SuppressWarnings("all")
 public class FileSyncJre {
   public static void sync(final File folder, final Node node) {
-    final Deferred<Success> _function = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> cb) {
-        FileItem _wrap = FilesJre.wrap(folder);
-        FileSync.sync(_wrap, node, cb);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function);
+    throw new Error("Unresolved compilation problems:"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context.");
   }
 }
--- src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
+++ src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
@@ -1,10 +1,6 @@
 package com.appjangle.filesync.tests;
 
-import com.appjangle.filesync.FileSync;
 import com.appjangle.jre.AppjangleJre;
-import de.mxro.async.Deferred;
-import de.mxro.async.callbacks.ValueCallback;
-import de.mxro.async.jre.AsyncJre;
 import de.mxro.file.FileItem;
 import de.mxro.file.Jre.FilesJre;
 import de.mxro.fn.Success;
@@ -61,22 +57,11 @@ public abstract class CheckFilesToNodesTemplate {
   
   @Test
   public void test() {
-    final Deferred<Success> _function = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> cb) {
-        FileSync.syncSingleFolder(CheckFilesToNodesTemplate.this.sourceFolder, CheckFilesToNodesTemplate.this.result, cb);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function);
-    this.step1_defineFiles();
-    final Deferred<Success> _function_1 = new Deferred<Success>() {
-      public void apply(final ValueCallback<Success> cb) {
-        FileSync.syncSingleFolder(CheckFilesToNodesTemplate.this.sourceFolder, CheckFilesToNodesTemplate.this.result, cb);
-      }
-    };
-    AsyncJre.<Success>waitFor(_function_1);
-    NextwebPromise<Success> _commit = this.session.commit();
-    _commit.get();
-    this.step2_assertNodes();
+    throw new Error("Unresolved compilation problems:"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nType mismatch: cannot convert from (ValueCallback<Success>)=>void to Operation<Object>"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or use the closures in a more specific context.");
   }
   
   @After

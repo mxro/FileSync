cf54ecbe0db3832680508253a5aee6f169565a9f
mxro <mxro@nowhere.com>
2015-07-05 17:48:41 +1000

    d

--- src/main/src/main/java/com/appjangle/filesync/FileOperation.java
+++
@@ -1,8 +0,0 @@
-package com.appjangle.filesync;
-
-import com.appjangle.filesync.FileOperationContext;
-import de.mxro.fn.Closure;
-
-@SuppressWarnings("all")
-public interface FileOperation extends Closure<FileOperationContext> {
-}

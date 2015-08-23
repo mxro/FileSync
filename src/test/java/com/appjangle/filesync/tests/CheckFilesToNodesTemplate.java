5e406f97e61a6d99005dd1fe6584f21e03b29cda
mxro <mxro@nowhere.com>
2015-08-23 15:43:44 +1000

    Implementing ObjectHub JS framework Autocommit Sun Aug 23 15:43:44 EST 2015

--- src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
+++ src/test/java/com/appjangle/filesync/tests/CheckFilesToNodesTemplate.java
@@ -2,20 +2,15 @@ package com.appjangle.filesync.tests;
 
 import com.appjangle.api.Client;
 import com.appjangle.api.Node;
-import com.appjangle.api.Query;
 import com.appjangle.api.common.LocalServer;
-import com.appjangle.api.servers.Servers;
 import com.appjangle.filesync.FileSync;
-import com.appjangle.jre.Clients;
 import de.mxro.file.FileItem;
-import de.mxro.file.Jre.FilesJre;
 import delight.async.Operation;
 import delight.async.callbacks.ValueCallback;
 import delight.async.jre.Async;
 import delight.functional.Success;
 import io.nextweb.promise.NextwebPromise;
 import java.io.File;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -39,21 +34,9 @@ public abstract class CheckFilesToNodesTemplate {
   
   @Before
   public void setUp() {
-    try {
-      LocalServer _createAndStart = Servers.createAndStart();
-      this.server = _createAndStart;
-      Client _create = Clients.create(this.server);
-      this.session = _create;
-      Query _seed = this.session.seed(this.server);
-      Node _get = _seed.get();
-      this.result = _get;
-      File _newFolder = this.tempFolder.newFolder("sync1");
-      this.sourceFolder = _newFolder;
-      FileItem _wrap = FilesJre.wrap(this.sourceFolder);
-      this.source = _wrap;
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
-    }
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field Servers is undefined for the type CheckFilesToNodesTemplate"
+      + "\ncreateAndStart cannot be resolved");
   }
   
   protected abstract void step1_defineFiles();
